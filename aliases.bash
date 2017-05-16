14b8ff16680ac7ddd806fc117b79fd944465107e
dg <dg@shadowrobot.com>
2017-05-16 12:32:52 +0100

    changes

--- aliases.bash
+++ aliases.bash
@@ -1,6 +1,11 @@
 alias editdgbash='emacs ~/.dg-bash-config/*.bash'
 alias editbashrc='emacs ~/.bashrc & disown'
 alias sourcebashrc='source ~/.bashrc'
+
+alias emacsd='emacs $1 & disown'
+alias emacsc='emacsclient -n'
+
+
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../../"
@@ -9,3 +14,5 @@ alias cpr='cp -r'
 
 alias psros='ps aux | grep ros'
 alias rossrc='roscd ; cd ../src'
+
+alias l='ls -l -a -h'
--- catkin_here.bash
+++ catkin_here.bash
@@ -19,3 +19,26 @@ function catkin_here {
     catkin_make
     cd $startdir
 }
+
+function catkin_here_clean {
+    local startdir
+    local go
+    local count
+    local end
+    startdir=$(pwd)
+    go=1
+    while [ $go == 1 ]
+    do
+	count=$(pwd | grep -c '/src/')
+	end=$(pwd | grep -c src$)
+	if [ $count == 0 ] && [ $end == 0 ]
+	then
+	    go=0
+	else
+	    cd ..
+	fi
+    done
+    rm -r build/ devel/
+    catkin_make
+    cd $startdir
+}
