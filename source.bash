bd2ae88e288faa7c9552226b5dbe0bcb4ac0e62f
dg-shadow <dg@shadowrobot.com>
2017-05-23 15:42:12 +0100

    added link creation for emacs and git config

--- source.bash
+++ source.bash
@@ -1,3 +1,11 @@
 source ~/.dg-bash-config/aliases.bash
 source ~/.dg-bash-config/catkin_here.bash
 source ~/.dg-bash-config/.bash-git-prompt/gitprompt.sh
+
+if ! [ -e ~/.emacs ]; then
+    ln -s  ~/.dg-bash-config/.emacs ~/
+fi
+
+if ! [ -e ~/.gitconfig ]; then
+    ln -s  ~/.dg-bash-config/.gitconfig ~/
+fi
