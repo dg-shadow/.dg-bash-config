3372c874da316118a1c64863377465644c1e25a0
dg-shadow <dg@shadowrobot.com>
2017-05-23 15:49:57 +0100

    hostnames in docker aliases

--- aliases.bash
+++ aliases.bash
@@ -20,6 +20,6 @@ alias rossrc='roscd ; cd ../src'
 
 alias l='ls -l -a -h'
 
-alias docker_new_with_networking='f() { docker run -it --privileged $1 --hostname=hestia_docker --network=host -e DISPLAY -e QT_X11_NO_MITSHM=1 -e LOCAL_USER_ID=$(id -u) --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" shadowrobot/dexterous-hand:kinetic ; }; f '
+alias docker_new_with_networking='f() { docker run -it --privileged $1 --network=host -e DISPLAY -e QT_X11_NO_MITSHM=1 -e LOCAL_USER_ID=$(id -u) --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" shadowrobot/dexterous-hand:kinetic ; }; f '
 
-alias docker_new_no_networking='f() { docker run -it --host_name=hestia_docker $1 -e DISPLAY -e QT_X11_NO_MITSHM=1 -e LOCAL_USER_ID=$(id -u) --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" shadowrobot/dexterous-hand:kinetic ; }; f'
+alias docker_new_no_networking='f() { docker run -it $1 -e DISPLAY -e QT_X11_NO_MITSHM=1 -e LOCAL_USER_ID=$(id -u) --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" shadowrobot/dexterous-hand:kinetic ; }; f'
