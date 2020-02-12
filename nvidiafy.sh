f98407388d95d54c1972190ed41051ab7661deb2
dg-shadow <dg@shadowrobot.com>
2020-02-12 11:00:38 +0000

    added nvidiafication

--- nvidiafy.sh
+++ nvidiafy.sh
@@ -14,7 +14,7 @@ echo "FROM $docker_image" > Dockerfile
 echo "LABEL com.nvidia.volumes.needed=\"nvidia_driver\"" >> Dockerfile
 echo "ENV PATH /usr/local/nvidia/bin:\${PATH}" >> Dockerfile
 echo "ENV LD_LIBRARY_PATH /usr/local/nvidia/lib:/usr/local/nvidia/lib64:\${LD_LIBRARY_PATH}" >> Dockerfile
-echo "RUN apt update && apt -fy install bash-completion emacs gnome-icon-theme && cd /home/user && git clone https://github.com/dg-shadow/.dg-bash-config && chown -R user:user .dg-bash-config " >> Dockerfile
+
 
 
 
