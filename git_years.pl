25491a7623088505b2dddfb9ce40c4684ac72854
dg-shadow <dg@shadowrobot.com>
2022-11-01 10:13:56 +0000

    years

--- git_years.pl
+++ git_years.pl
@@ -12,6 +12,7 @@ chomp(my @files = <>) ;
 
 for (@files)
 {
+    my $file = $_;
     my %years;
     chomp (my @lines = split (/\n/, `git log $_ | grep Date`));
 
@@ -24,13 +25,39 @@ for (@files)
     }
     if (@lines)
     {
-        my $new_years = join(", ", sort(keys(%years)));
+        my @years = sort(keys(%years));
+        my @out_years;
+
+        while (@years)
+        {
+            my $run = 0;
+            while ($run < @years && $years[$run + 1] == $years[0] + $run + 1)
+            {
+                $run++
+            }
+            if ($run >= 2)
+            {
+                push(@out_years, "$years[0]-$years[$run]");
+                for (0..$run)
+                {
+                    shift(@years);
+                }
+            }
+            else
+            {
+                push(@out_years, shift(@years));
+            }
+        }
+
+
+        my $new_years = join(", ", @out_years); #sort(keys(%years)));
         my $old = `grep -i "Copyright (C)" $_`;
         $old =~ /(copyright \(c\) )(.*?)( shadow)/i;
+        print "$file\n";
         `sed -i 's/$1$2$3/$1$new_years$3/' $_`;
-            
+
     }
-        
+
 }
 
 #
